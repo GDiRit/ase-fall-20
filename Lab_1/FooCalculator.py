215396856da9847ef37c3fa61b6d7d35d2dfc6ba
Gianmaria <g.dirito@studenti.unipi.it>
2020-09-25 08:50:05 +0200

    second commit

--- Lab_1/FooCalculator.py
+++ Lab_1/FooCalculator.py
@@ -1,4 +1,4 @@
-import Calculator as c 
+import calculator as c 
 
 class FooCalculator:
 
@@ -9,6 +9,14 @@ class FooCalculator:
     def sum(self, m, n):
         return c.sum(m, n)
     
+    def subtract(self, m, n):
+        return c.subtract(m, n)
+
+    def multiply(self, m, n):
+        return c.multiply(m, n)
+
     def divide(self, m, n):
         return c.divide(m, n)
     
+    def gcd(self, m, n):
+        return c.gcd(m, n)
